447e6a71c644cc1e3a4d92f3c1edf60cfa728110
Nikesh Mistry <Nikesh Mistry>
2016-09-11 19:06:10 +0100

    Update resume sections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,18 +3,52 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 //#$("#main").append("Nikesh");
 
-var firstName = "Nikesh";
-var age = 22;
-var role = "Consultant";
+//var firstName = "Nikesh";
+//var age = 22;
+//var role = "Consultant";
+//var skills = ["JS","Git"];
+var bio ={
+	"name": "Nikesh M",
+	"role": "Consultant",
+	"welcomeMessage": "Welcome to my resume",
+	"biopic": "images/fry.jpg",
+	"contacts":{
+		"mobile": "02546868",
+		"email" : "me@me.me",
+		"github": "blah",
+		"twitter": "@blah",
+		"location": "London"
+	},
+	"skills" : ["Git","JS","Html"]
+};
+
+var work ={
+	"Position" : "Consultant",
+	"Employer" : "Tesco",
+	"Years worked" : "5",
+	"City" : "London"
+};
+
+var education = {
+	"schools":[
+		{
+			"School" : "Woodlands",
+			"Years" : 15,
+			"City" : "Birmingham"
+		},
+		{
+
+			"School" : "Tilehill",
+			"Years" : 1,
+			"City" : "London"
+		}
+	]	
+}
 
 
-//var awesomeThoughts = "I am Nikesh and I am Awesome";
-//console.log(awesomeThoughts);
+//var formattedName = HTMLheaderName.replace("%data%",bio.name);
+//var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
 
-//var funThoughts = awesomeThoughts.replace("Awesome","Fun")
-//$("#main").append(funThoughts);
-
-var formattedName = HTMLheaderName.replace("%data%",firstName);
-var formattedRole = HTMLheaderRole.replace("%data%",role);
-
-$("#header").prepend(formattedName,formattedRole);
+//$("#header").prepend(formattedName,formattedRole);
+//$("#main").append(work.Position);
+//$("#main").append (education["School"]);
