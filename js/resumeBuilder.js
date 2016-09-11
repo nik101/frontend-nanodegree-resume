51c0c5de11bff70f63b2ccbabd2e3ea5e1881348
Nik <nikeshmistry15@gmail.com>
2016-09-11 17:29:47 +0100

    latest changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+//#$("#main").append("Nikesh");
+
+var firstName = "Nikesh";
+var age = 22;
+var role = "Consultant";
+
+
+//var awesomeThoughts = "I am Nikesh and I am Awesome";
+//console.log(awesomeThoughts);
+
+//var funThoughts = awesomeThoughts.replace("Awesome","Fun")
+//$("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%",firstName);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+$("#header").prepend(formattedName,formattedRole);
